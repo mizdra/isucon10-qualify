9b17a2bc59bea4a1d13d29a182a0642aeed7ab52
mizdra <pp.mizdra@gmail.com>
2021-07-24 21:51:42 +0900

    デプロイスクリプト追加

---
+++ tools/deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -ex
+
+ROOT=$(cd $(dirname $0)/..;pwd)
+
+
+echo "start build"
+cd $ROOT/go
+GOOS=linux make -B
+
+
+echo "start deploy ${USER}"
+for server in isu01; do
+    ssh -t $server "sudo systemctl stop isuumo.go.service"
+    ssh -t $server "sudo systemctl stop nginx.service"
+    ssh -t $server "sudo systemctl stop mysql.service"
+
+    rsync -a --rsync-path="sudo -u isucon rsync" $ROOT/go/isuumo $server:/home/isucon/isuumo/webapp/go/isuumo
+    rsync -a --rsync-path="sudo rsync" $ROOT/nginx/sites-available/ $server:/etc/nginx/sites-available/
+    rsync -a --rsync-path="sudo rsync" $ROOT/nginx/nginx.conf $server:/etc/nginx/nginx.conf
+    rsync -a --rsync-path="sudo -u isucon rsync" $ROOT/env.sh $server:/home/isucon/env.sh
+    rsync -a --rsync-path="sudo rsync" $ROOT/mysql/conf.d/ $server:/etc/mysql/conf.d/
+    rsync -a --rsync-path="sudo rsync" $ROOT/mysql/mysql.conf.d/ $server:/etc/mysql/mysql.conf.d/
+
+    ssh -t $server "sudo systemctl start isuumo.go.service"
+    ssh -t $server "sudo systemctl start nginx.service"
+    ssh -t $server "sudo systemctl start mysql.service"
+done
