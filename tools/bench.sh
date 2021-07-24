faeb98544fd59748cc2f43495b6ed251c83ebdf2
mizdra <pp.mizdra@gmail.com>
2021-07-24 23:25:51 +0900

    ログ削除は bench.sh でやる

--- tools/bench.sh
+++ tools/bench.sh
@@ -3,4 +3,10 @@ set -ex
 
 ROOT=$(cd $(dirname $0)/..;pwd)
 
+echo "clear logs"
+for server in isu01; do
+  ssh -t $server "sudo rm -f /var/log/nginx/access.log"
+  ssh -t $server "sudo rm -f /var/log/nginx/error.log"
+done
+
 ssh -t bench "cd /home/isucon/isuumo/bench; /home/isucon/isuumo/bench/bench --target-url http://18.183.153.63"
